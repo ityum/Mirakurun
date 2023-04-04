346dafce5abdf067b6922b45c2d0f53902c71d53
stuayu <60843145+stuayu@users.noreply.github.com>
2023-04-04 19:29:57 +0900

    チューナーオープン失敗時の再試行回数を50回から5回に変更 再オープンするまで250msから1s間隔に変更 Windows環境下でチューナーからの応答がなくなる原因解消を目的

--- src/Mirakurun/Tuner.ts
+++ src/Mirakurun/Tuner.ts
@@ -286,7 +286,8 @@ export default class Tuner {
 
             const devices = this._getDevicesByType(setting.channel.type);
 
-            let tryCount = 50;
+            let tryCount = 5;
+            let wait_tuner_ms = 1000; // ms default(1s)
             const length = devices.length;
 
             function find() {
@@ -360,7 +361,8 @@ export default class Tuner {
                 if (device === null) {
                     --tryCount;
                     if (tryCount > 0) {
-                        setTimeout(find, 250);
+                        setTimeout(find, wait_tuner_ms);
+                        wait_tuner_ms += 1000; // チューナーオープンに失敗するたびに再オープンまで+1秒する
                     } else {
                         reject(new Error("no available tuners"));
                     }
